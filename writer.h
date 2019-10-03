b915a4cf968818ea4b675bde4b88216c34eecb9f
anthonyjamesgoddard <antg3254@gmail.com>
2019-10-03 10:57:33 +0100

    added thread safe writer @

---
+++ writer.h
@@ -0,0 +1,53 @@
+#ifndef WRIT_H
+#define WRIT_H
+
+#include <mutex>
+#include <ostream>
+#include <string>
+
+#include "gnss_synchro.pb.h"
+#include "monitor_pvt.pb.h"
+#include "sat_pvt.pb.h"
+
+class Writer {
+    mutable std::mutex m;
+    std::ostream* m_out;
+
+public:
+    Writer() : m_out{&std::cout} {};
+
+    void write(std::map<int, gnss_sdr::GnssSynchro>& channels) {
+
+        {
+            std::lock_guard<std::mutex> lk(m);
+            // locked now we can write
+            std::ostream& output = *m_out;
+            for (const auto& c : channels) {
+                // clang-format off
+                auto sync = c.second;
+                output << c.first << " "
+                       << sync.very_early()
+                       << '\n';
+            }
+            output << std::endl;
+        }
+        // unlocked
+    }
+
+    void write(gnss_sdr::MonitorPvt& monitor) {
+
+        {
+            std::lock_guard<std::mutex> lk(m);
+            std::ostream& output = *m_out;
+            output << "monitor" << monitor.pos_x() << std::endl;
+        }
+    }
+
+    void write(gnss_sdr::SatPvt& sat) {
+        {
+
+        }
+    }
+};
+
+#endif
